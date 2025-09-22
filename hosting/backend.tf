935cc64fbc9c90f4adf4cd496c9871d3a5d0829b
Curtis Lawhorn <curtis@lawhorns.com>
2025-09-21 19:57:33 -0500

    Removed network setup components

--- hosting/backend.tf
+++ hosting/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "us-east-2"
     bucket         = "curtislawhorn-terraform-states"
-    key            = "prod/api-gateway/terraform.tfstate"
+    key            = "weather-services/terraform.tfstate"
     dynamodb_table = "curtislawhorn-terraform-locks"
     encrypt        = true
   }
